65a6d8cec28cf1dd6595ab1be801a2a69bbc6065
nyaous555 <>
2025-12-11 06:00:44 +0900

    laravel relation

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,24 +3,30 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    // 一覧表示
     public function index() {
         $posts = Post::all();
         return view('posts.index', compact('posts'));
     }
 
+    // 登録画面表示
     public function create() {
         return view('posts.create');
     }
 
+    //新規登録処理
     public function store(Request $request) {
         $validated = $request->validate([
             'title' => 'required | max:20',
             'body' => 'required | max:400',
         ]);
+        // dd(auth());
+        $validated['user_id'] = auth()->id();
 
         $post = Post::create($validated);
         // バリデーション処理前の記述
